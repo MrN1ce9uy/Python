5430d66a9e6ccbbf50987d6b53710ee0c6209995
Iwan in 't Groen <iwanintgroen@gmail.com>
2019-07-13 19:04:21 +0200

    Cleanup

--- main.py
+++ main.py
@@ -1,40 +1,47 @@
-#This is a simple program that utilizes variables, functions, & a loop.
-#Created by MrN1ce9uy
+# This is a simple program that utilizes variables, functions, & a loop.
 
-#Define payroll function
+
+# Define payroll function
 def payroll():
-	#Payroll calculations
-	hours = float(input("Enter number of hours: "))
-	rate = float(input("Enter hourly rate: "))
-	amount = hours * rate
-	#Print values
-	print ("Hours worked:", hours)
-	print ("Rate:", rate)
-	print ("Pay amount:", amount)
+    # Payroll calculations
+    hours = float(input("Enter number of hours: "))
+    rate = float(input("Enter hourly rate: "))
 
-#Define mileage function
+    amount = hours * rate
+
+    # Print values
+    print("Hours worked:", hours)
+    print("Rate:", rate)
+    print("Pay amount:", amount)
+
+
+# Define mileage function
 def mileage():
-	#MPG calculations
-	miles = float(input("Enter number of miles: "))
-	gas = float(input("Enter gallons of gas: "))
-	mpg = miles / gas
-	#Print values
-	print ("Miles:", miles)
-	print ("Gas:", gas)
-	print ("MPG:", mpg)
+    # MPG calculations
+    miles = float(input("Enter number of miles: "))
+    gas = float(input("Enter gallons of gas: "))
 
-#Define variable
-menuSelection = 0
+    mpg = miles / gas
 
-#Decision loop
-while menuSelection != 3:
-	#Display options to the user and record their input
-	print("Press 1 for Payroll Calculation")
-	print("Press 2 for Mileage Calculation")
-	print("Press 3 to Exit")
-	menuSelection = int(input())
-	#if-else statement
-	if menuSelection == 1:
-		payroll()
-	elif menuSelection == 2:
-		mileage()
+    # Print values
+    print("Miles:", miles)
+    print("Gas:", gas)
+    print("MPG:", mpg)
+
+
+# Decision loop
+while True:
+    # Display options to the user and record their input
+    print("Press 1 for Payroll Calculation")
+    print("Press 2 for Mileage Calculation")
+    print("Press 3 to Exit")
+
+    menu_selection = int(input())
+
+    # if-else statement
+    if menu_selection == 1:
+        payroll()
+    elif menu_selection == 2:
+        mileage()
+    elif menu_selection == 3:
+        break
