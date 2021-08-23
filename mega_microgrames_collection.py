ad8c9a65327abae7879997f2ebe15c03da211bb8
MrN1ce9uy <35491501+MrN1ce9uy@users.noreply.github.com>
2021-08-23 05:52:36 -0500

    Update mega_microgrames_collection.py

--- mega_microgrames_collection.py
+++ mega_microgrames_collection.py
@@ -4,9 +4,9 @@
 
 from Tkinter import *
 
-import rock_paper_scissors2
+import rockpaperscissors
 import hangman
-import poker_dice
+import pokerdice
 
 root = Tk()
 root.title ("Linux User & Developer's Mega Microgrames Collection")
@@ -15,12 +15,21 @@ mainframe = Frame(root, height = 200, width = 500)
 mainframe.pack_propagate(0)
 mainframe.pack(padx = 5, pady = 5)
 
-intro = Label(mainframe, text = """Welcome to Linux User & Developers Mega Microgrames Collection. Please select one of the following games to play:
+intro = Label(mainframe, text = """Welcome to Linux User & Developers Mega Microgrames Collection.
+Please select one of the following games to play:
 """)
 intro.pack(side = TOP)
 
+rps_button = Button(mainframe, tet = "Rock, Paper, Scissors", command = rockpaperscissors.gui)
 rps_button.pack()
 
-hm_button = Button(mainframe, tet = "Rock, Paper, Scissors", command = rockpaperscissors.gui)
-rps_button.pack()
+rps_button = Button(mainframe, text = "Hangman", command = hangman.start)
+hm_button.pack()
 
+pd_button = Button(mainframe, text = "Poker Dice", command = pookerdice.start)
+pd_button.pack()
+
+exit_button = Button(mainframe, text = "quit", command = root.destroy)
+exit_button.pack(side = BOTTOM)
+
+root.mainloop()
