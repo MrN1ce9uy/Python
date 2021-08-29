b212844d54b88c102667d85ae05de6b575b3c5cd
MrN1ce9uy <35491501+MrN1ce9uy@users.noreply.github.com>
2021-08-29 14:52:57 -0500

    Add files via upload

---
+++ myturtle.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python3
+
+from turtle import *
+shape('turtle')
+
+# Makes an equilateral triangle
+forward(100)
+left(120)
+forward(100)
+left(120)
+forward(100)
+
+# Makes a square on top of the triangle above
+left(120)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
