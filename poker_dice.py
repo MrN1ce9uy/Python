43db6304514dafdf23a637da2111d98a81852b96
MrN1ce9uy <35491501+MrN1ce9uy@users.noreply.github.com>
2022-08-07 18:55:54 -0500

    Add files via upload

--- poker_dice.py
+++ poker_dice.py
@@ -1,5 +1,3 @@
-#! /usr/bin/python2
-
 import random
 from itertools import groupby
 
@@ -16,13 +14,13 @@ player_score = 0
 computer_score = 0
 
 def start():
-    print "Let's play a game of Linux Poker Dice."
+    print("Let's play a game of poker.")
     while game():
         pass
     scores()
 
 def game():
-    print "The computer will help you throw your 5 dice"
+    print("The computer will help you throw your 5 dice.")
     throws()
     return play_again()
 
@@ -31,26 +29,27 @@ def throws():
     dice = roll(roll_number)
     dice.sort()
     for i in range(len(dice)):
-        print "Dice",i + 1,":",names[dice[i]]
+        print("Dice", i + 1,":",names[dice[i]])
 
-    result = hand(dice)
-    print "You currently have", result
+        result = hand(dice)
+        print("You currently have", result)
 
-    while True:
-        rerolls = input("How many dice do you want to throw again? ")
-        try:
-            if rerolls in (1,2,3,4,5):
-                break
-        except ValueError:
-            pass
-        print "Oops! I didn't understand that. Please enter 1, 2, 3, 4 or 5."
+        while True:
+            rerolls = input("How many dice do you want to throw again? ")
+            try:
+                if rerolls in (1,2,3,4,5):
+                    break
+            except ValueError:
+                pass
+            print("Oops! I didn't understand that. Please enter 1, 2, 3, 4, or 5.")
+
     if rerolls == 0:
-        print "You finish with", result
+        print("You finish with", result)
     else:
         roll_number = rerolls
         dice_rerolls = roll(roll_number)
         dice_changes = range(rerolls)
-        print "Enter the number of a dice to reroll: "
+        print("Enter the number of a dice to reroll: ")
         iterations = 0
         while iterations < rerolls:
             iterations = iterations + 1
@@ -61,22 +60,22 @@ def throws():
                         break
                 except ValueError:
                     pass
-                print "Oops! I didn't understand that. Please enter 1, 2, 3, 4 or 5."
+                print("Oops! I didn't understand that. Plase enter 1, 2, 3, 4, or 5.")
             dice_changes[iterations-1] = selection-1
-            print "You have changed dice", selection
+            print("You have changed dice", selection)
 
         iterations = 0
         while iterations < rerolls:
             iterations = iterations + 1
             replacement = dice_rerolls[iterations-1]
             dice[dice_changes[iterations-1]] = replacement
-
+            
         dice.sort()
         for i in range(len(dice)):
-            print "Dice",i + 1,":",names[dice[i]]
+            print("Dice", i + 1,":",names[dice[i]])
 
         result = hand(dice)
-        print "You finish with", result
+        print("You finish with", result)            
 
 def roll(roll_number):
     numbers = range(1,7)
@@ -100,7 +99,7 @@ def hand(dice):
     elif dice_hand[0] == 4:
         return "four of a kind!"
     elif dice_hand[0] == 3:
-        if dice_hand[1] == 2:
+        if dice_hand[0] == 2:
             return "a full house!"
         else:
             return "three of a kind."
@@ -113,17 +112,17 @@ def hand(dice):
         return "a high card."
 
 def play_again():
-    answer = raw_input("Would you like to play again? y/n: ")
-    if answer in ("y", "Y", "yes", "yes", "of course!", "Of course!"):
+    answer = input("Would you like to play again? y/n: ")
+    if answer in ("y", "Y", "yes", "Yes", "Of course!"):
         return answer
     else:
-        print "Thank you very much for playing our game. See you next time!"
+        print("Thank you very much for playing my game. See you next time!")
 
 def scores():
     global player_score, computer_score
-    print "HIGH SCORES"
-    print "Player: ", player_score
-    print "Computer: ", computer_score
+    print("HIGH SCORES")
+    print("Player: ", player_score)
+    print("Computer: ", computer_score)
 
 if __name__ == '__main__':
     start()
